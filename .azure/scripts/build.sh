20ebaf0af55ff72d14617153c81aa0b889e51702
Jakub Scholz <www@scholzj.com>
2022-11-11 17:09:39 +0100

    Add Java 17 and remove Java 8 (#15)
    * Add Java 17 and remove Java 8
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    * Review comments JS
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .azure/scripts/build.sh
+++ .azure/scripts/build.sh
@@ -6,14 +6,16 @@ echo "Source branch: ${BRANCH}"
 
 # The first segment of the version number is '1' for releases < 9; then '9', '10', '11', ...
 JAVA_MAJOR_VERSION=$(java -version 2>&1 | sed -E -n 's/.* version "([0-9]*).*$/\1/p')
-if [ ${JAVA_MAJOR_VERSION} -eq 1 ] ; then
+if [ "${JAVA_MAJOR_VERSION}" -eq "11" ] ; then
   # some parts of the workflow should be done only one on the main build which is currently Java 11
   export MAIN_BUILD="TRUE"
   echo "Running main build"
 fi
 
 # Build with Maven
+# shellcheck disable=SC2086
 mvn $MVN_ARGS install
+# shellcheck disable=SC2086
 mvn $MVN_ARGS spotbugs:check
 
 # Push to Nexus

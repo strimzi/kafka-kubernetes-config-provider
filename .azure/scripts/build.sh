fa76f35f15c01954f5e1f6d4c3fef0a5a87f4f36
Lukáš Král <53821852+im-konge@users.noreply.github.com>
2025-06-19 11:31:23 +0200

    Add release pipeline (#42)
    * Add release pipeline
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * fixes
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    ---------
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- .azure/scripts/build.sh
+++ .azure/scripts/build.sh
@@ -21,11 +21,11 @@ mvn $MVN_ARGS spotbugs:check
 # Push to Nexus
 if [ "$BUILD_REASON" == "PullRequest" ] ; then
     echo "Building Pull Request - nothing to push"
-elif [[ "$BRANCH" != "refs/tags/"* ]] && [ "$BRANCH" != "refs/heads/main" ]; then
-    echo "Not in main branch or in release tag - nothing to push"
+elif [ "$BRANCH" != "refs/heads/main" ]; then
+    echo "Not in main branch - nothing to push"
 else
    if [ "${MAIN_BUILD}" = "TRUE" ] ; then
        echo "In main branch or in release tag - pushing to nexus"
-       ./.azure/scripts/push-to-nexus.sh
+       ./.azure/scripts/push-to-central.sh
    fi
 fi
